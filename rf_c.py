c877e78c46453499f6a7e345534152d8505fba52
euph0x1 <ahmedfaria99@gmail.com>
2026-08-03 19:54:11 +0530

    ml

--- rf_c.py
+++
@@ -1,9 +0,0 @@
-from sklearn.datasets import load_breast_cancer
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.metrics import classification_report, accuracy_score
-import pandas as pd
-import numpy as np
-import matplotlib.pyplot as plt
-data=load_breast_cancer(as_frame=True)
-df=data.frame
-print(df.head())
